e69eb4a408836b6d136adc0be08cfdcdc66d7d3f
Kelvin Louis <kelvin.louis@yoo.digital>
2020-07-16 14:47:36 +0200

    fix: Adjust dependencies and rules
    - Define fixed version of react eslint plugin, because of a bug introduced in version 7.20.3
    - Allow functional components to exceed 50 lines of code
    - Allow JSX spreading
    
    #GUILD-66

--- packages/eslint-config-react/index.js
+++ packages/eslint-config-react/index.js
@@ -30,5 +30,28 @@ module.exports =  {
       'error',
       { extensions: ['.ts', '.tsx'] },
     ],
+
+    'react/jsx-props-no-spreading': 'off',
   },
+  overrides: [
+    /**
+     * Unfortunately there is no way of avoiding the rule "max-lines-per-function"
+     * for functional components (or any render function).
+     * This override is not perfect, as it ignores all other functions declared in
+     * the listed directories as well...
+     *
+     * See: https://github.com/eslint/eslint/issues/12236
+     */
+    {
+      files: [
+        'src/components/**',
+        'src/hooks/**',
+        'src/pages/**',
+        'src/layouts/**',
+      ],
+      rules: {
+        'max-lines-per-function': 'off',
+      },
+    },
+  ],
 };
