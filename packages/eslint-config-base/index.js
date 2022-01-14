c6bf8f8a0ed817aa38e42cb831f792d58dd599df
Sacha Schmid <sacha.schmid@yoo.digital>
2022-01-14 09:02:43 +0100

    fix(Linting): Add trailing comma #GUILD-141

--- packages/eslint-config-base/index.js
+++ packages/eslint-config-base/index.js
@@ -104,7 +104,7 @@ module.exports =  {
           'private-instance-method',
           'public-abstract-method',
           'protected-abstract-method',
-          'private-abstract-method'
+          'private-abstract-method',
         ],
       },
     }],
